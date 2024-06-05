2a1e994bd33bbce9cefff11b2f7974bffb51adf1
SerialQi <chengxuanmei@gmail.com>
2024-06-05 12:12:50 +0800

    1

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=15028378&assignment_repo_type=AssignmentRepo)
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+This project delves into the world of AI-driven image processing and custom filters, with a focus on highlighting elements within photographs using computational algorithms.  I was inspired by a group photos i took in Queenstown about sheep shearing, I found that was really intersting.
+
+The images above are photos of shaving sheep algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The mask in each photo highlights the sheep's process of being shaved, which is cute and funny.Using a modified version of the p5.js Pointillism example, I set out to algorithmically process the photos. Each image became a canvas for experimentation, as I fine-tuned masking techniques and algorithm parameters to achieve the desired visual impact.
+
--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
-
-// change these three lines as appropiate
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
+let curLayer = 0; 
+// change these three lines as appropriate
 let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
+let maskFile = "mask_1.png";
 let outputFile = "output_1.png";
 
 function preload() {
@@ -12,35 +12,73 @@ function preload() {
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(1920, 1080);
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
   noStroke();
-  background(255, 0, 0);
+  //background(0,0,0);
+  background(231, 221, 255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
+  
 }
-
 function draw () {
-  for(let i=0;i<4000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 10;
-      ellipse(x, y, pointSize, pointSize);
+  if (curLayer == 0) {
+    /////// gray image first layer 
+    let num_lines_to_draw = 10;
+    for (let j = renderCounter; j < renderCounter + num_lines_to_draw && j < 1080; j++) {
+      for (let i = 0; i < 1920; i++) {
+        colorMode(RGB);
+        let pix = sourceImg.get(i, j);
+        let col = color(pix);
+        let gray = (red(col) + green(col) + blue(col)) / 3 
+        let grayColor = color(gray, gray, gray);
+        set(i, j, grayColor);
+      }
     }
-    else {
-      let pointSize = 20;
-      rect(x, y, pointSize, pointSize);    
+    renderCounter += num_lines_to_draw
+    updatePixels();
+   ///  sheep 
+   drawSheep()
+
+    if(renderCounter >= 1080){
+      curLayer = 1
+      renderCounter = 0
     }
+    /// second layer 
+  } else if(curLayer == 1){
+    rectMode(CORNERS);
+    for(let i=0; i<100; i++) {
+      let x1 = random(0, width);
+      let y1 = random(0, height);
+      let x2 = x1 + random(-10, 10);
+      let y2 = y1 + random(-10, 10);
+      colorMode(RGB);
+      let pix = sourceImg.get(x1, y1);
+      let mask = maskImg.get(x1, y1);
+      let col = color(pix);
+      stroke(col);
+      fill(col);
+      if(mask[1] < 128) {
+        rect(x1, y1, x2, y2+20); //rectangle 
+      }
+      else {
+        let pointSize2 = 12
+        ellipse(x1, y1, pointSize2, pointSize2); //circle 
+      }
+    }
+
+    renderCounter = renderCounter + 1
+ 
+
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(curLayer == 0 && renderCounter > 1080) {
+    curLayer = 1;
+    renderCounter = 0;
+  }
+  else if(curLayer == 1 && renderCounter > 500) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
@@ -52,4 +90,60 @@ function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
   }
+}
+
+function drawSheep(x,y){
+  // sheep body
+  noStroke()
+  //sheep body
+   noStroke()
+  fill(255)
+  ellipse(200,200,150,100)
+  circle(200,140,50)
+  ellipse(160,140,50,40)
+  ellipse(140,160,50,40)
+  ellipse(120,200,80,70)
+  ellipse(140,240,60,40)
+  ellipse(200,245,70,50)
+  ellipse(250,220,70,50)
+  ellipse(260,190,70,60)
+  ellipse(230,160,70,60)
+
+//sheep face 
+  fill(0)
+  ellipse(160,180,65,50)
+
+//sheep ear 
+  push(); 
+  translate(140, 160); 
+  rotate(radians(160));
+  ellipse(0, 0, 30, 15); 
+  pop();
+
+  push(); 
+  translate(180, 160); 
+  rotate(radians(-170)); 
+  ellipse(0, 0, 30, 15); 
+  pop(); 
+
+// leg
+  push(); 
+  translate(130, 260); 
+  rotate(radians(150));
+  ellipse(0, 0, 30, 17); 
+  pop();
+
+  push(); 
+  translate(250, 245); 
+  rotate(radians(-120)); 
+  ellipse(0, 0, 30, 15); 
+  pop(); 
+
+//eye
+  fill(255)
+  circle(150,170,20)
+  circle(180,170,20)
+  fill(0)
+  circle(150,175,10)
+  circle(180,175,10)
 }
